2e334a806544c8cae3d9fee5702f07f45d405247
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-03-08 19:07:23 +0100

    update scripts (A_link.sh)

--- scripts/debian/saltstack_modules/A_link.sh
+++ scripts/debian/saltstack_modules/A_link.sh
@@ -7,16 +7,16 @@ set -x
 for d in /vagrant/salt/formulas/*; do
   src=${d}/states
   dst=/srv/salt/states/${d##*/}
-  [[ -e $src && ! -e $dst ]] && ln -s $src $dst
+  if [[ -e $src && ! -e $dst ]]; then ln -s $src $dst || exit 1; fi
 
   src=${d}/contrib/states
   dst=/srv/salt/contrib/states/${d##*/}
-  [[ -e $src && ! -e $dst ]] && ln -s $src $dst
+  if [[ -e $src && ! -e $dst ]]; then ln -s $src $dst || exit 1; fi
 
   src=${d}/pillar_examples
   dst=/srv/salt/pillar/${d##*/}
-  [[ -e $src && ! -e $dst ]] && ln -s $src $dst
+  if [[ -e $src && ! -e $dst ]]; then ln -s $src $dst || exit 1; fi
 done
 
-find /vagrant/salt/formulas/ -path '*/_modules/*' -type f -name '*.py' -exec cp {} /srv/salt/_modules/ \;
-[[ -d /vagrant/salt/_modules/ ]] && find /vagrant/salt/_modules/ -type f -name '*.py' -exec cp {} /srv/salt/_modules/ \;
+find /vagrant/salt/formulas/ -path '*/_modules/*' -type f -name '*.py' -exec cp {} /srv/salt/_modules/ \; || exit 1
+if [[ -d /vagrant/salt/_modules/ ]]; then find /vagrant/salt/_modules/ -type f -name '*.py' -exec cp {} /srv/salt/_modules/ \; || exit 1; fi
